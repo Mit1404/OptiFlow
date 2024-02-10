ead08a8ac20a2c7c24e883f47b7b172bfeb5aa73
akashm-2003 <akashmanna318@gmail.com>
2024-02-11 04:41:18 +0530

    React router implementation

--- src/pages/Sprint.jsx
+++ src/pages/Sprint.jsx
@@ -4,27 +4,42 @@ import {
   KanbanComponent,
 } from "@syncfusion/ej2-react-kanban";
 import React from "react";
-
 import { Header } from "../components";
 import { kanbanData, kanbanGrid } from "../data/dummy";
+import { IoAddSharp } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
-const Sprint = () => (
-  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-    <Header category="App-name  " title="Sprint" />
-    <KanbanComponent
-      id="kanban"
-      keyField="Status"
-      dataSource={kanbanData}
-      cardSettings={{ contentField: "Summary", headerField: "Id" }}
-    >
-      <ColumnsDirective>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        {kanbanGrid.map((item, index) => (
-          <ColumnDirective key={index} {...item} />
-        ))}
-      </ColumnsDirective>
-    </KanbanComponent>
-  </div>
-);
-
+const Sprint = () => {
+  const navigate = useNavigate();
+  
+  const addSprint = () => {
+    navigate("/sprintform");
+  };
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <div className="flex flex-row justify-between">
+        <Header category="App-name" title="Sprint" />
+        <IoAddSharp size="50px" onClick={addSprint} />
+      </div>
+      <KanbanComponent
+        id="kanban"
+        keyField="Status"
+        dataSource={kanbanData}
+        cardSettings={{
+          contentField: "Summary",
+          headerField: "Id",
+          tagsField: "Tags",
+          grabberField: "Color",
+        }}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {kanbanGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
+  );
+};
 export default Sprint;
